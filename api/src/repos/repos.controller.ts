0744fd0bbcac9d22bb332e0dd187a6704ea34eba
Julien RICHARD <julien.y.richard@gmail.com>
2025-03-05 15:09:41 +0100

    Mise en place du middleware de validation de données'

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -1,8 +1,32 @@
-import express, { Response, Request } from "express";
+import express, { Response, Request, NextFunction } from "express";
+import Joi from "joi";
 import data from "../../data.json";
 import { Repos } from "./repos.type";
 
 const repos = express.Router();
+
+const schema = Joi.object({
+  url: Joi.string().required(),
+  isPrivate: Joi.boolean().required(),
+  languages: Joi.array().items(
+    Joi.object({
+      size: Joi.number().required(),
+      node: Joi.object({
+        name: Joi.string(),
+      }),
+    })
+  ),
+});
+
+const validateRepo = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    res.status(422).json(error);
+  } else {
+    next();
+  }
+};
 
 /**
  * Route GET pour récupérer tous mes repos
@@ -26,7 +50,7 @@ repos.get("/:reposid", (req: Request, res: Response) => {
   }
 });
 
-repos.post("/", (req, res) => {
+repos.post("/", validateRepo, (req: Request, res: Response) => {
   const newId = Math.ceil(Math.random() * 100000).toString();
   data.push({ ...req.body, id: newId });
   res.status(201).json({ id: newId });
