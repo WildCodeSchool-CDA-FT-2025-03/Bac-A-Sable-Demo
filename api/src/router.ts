bf1f5ec83808289b84a6c51a17c7e9860d6935dd
Julien RICHARD <julien.y.richard@gmail.com>
2025-03-05 10:06:06 +0100

    Typescript sur la route

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express, { Response } from "express";
 
 const router = express.Router();
 
 // Route en API REST Verb HTTP + uri (/api/repos/12)
 // GET, POST, PUT, PATCH DELETE
 // http://localhost:3000/api/repos
-router.get("/", (req, res) => {
+router.get("/", (_, res: Response) => {
   res.status(200).send("Tout est OK");
 });
 
