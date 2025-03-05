fef3739cbfbac4920e4b516ff84eec3367f847ca
Julien RICHARD <julien.y.richard@gmail.com>
2025-03-05 10:21:02 +0100

    Mise en place du controller pour mes repos

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,4 +1,5 @@
 import express, { Response } from "express";
+import repos from "./repos/repos.controller";
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ const router = express.Router();
 router.get("/", (_, res: Response) => {
   res.status(200).send("Tout est OK");
 });
+
+router.use("/repos", repos);
 
 export default router;
