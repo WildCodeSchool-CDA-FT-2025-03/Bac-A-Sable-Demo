5995b2211fbcdd826106eb8aef6c21396780b560
Julien RICHARD <julien.y.richard@gmail.com>
2025-03-05 10:47:48 +0100

    Mise en place fonctionnelle de la route repos/:id

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -3,8 +3,20 @@ import data from "../../data.json";
 
 const repos = express.Router();
 
+/**
+ * Route GET pour récupérer tous mes repos
+ *  /api/repos
+ */
 repos.get("/", (_, res: Response) => {
   res.status(200).json(data);
 });
 
+/**
+ * Route Get pour récupérer un repos via son id
+ * /api/repos/dujen_deodei
+ */
+repos.get("/:reposid", (req, res) => {
+  const repo = data.find((rep) => rep.id === req.params.reposid);
+  res.status(200).json(repo);
+});
 export default repos;
