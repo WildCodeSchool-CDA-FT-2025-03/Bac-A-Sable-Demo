817173c51436f606b44d949773d4168544f19bca
Julien RICHARD <julien.y.richard@gmail.com>
2025-03-06 09:54:31 +0100

    Ajout du querys params de limit pour pagination

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -13,9 +13,14 @@ repos.get("/", (req: Request, res: Response) => {
   console.log("Hit all repo controller");
   console.log(req.query);
 
-  const result = req.query.isPrivate
+  // Select * from repos where isPrivate = ?, [req.query.isPrivate]
+  let result = req.query.isPrivate
     ? data.filter((rep) => rep.isPrivate.toString() === req.query.isPrivate)
     : data;
+
+  if (req.query.limit) {
+    result = result.slice(0, +req.query.limit);
+  }
   res.status(200).json(result);
 });
 
