abc496e6288e4409a6ffc8a1dfc53898eace0d60
Julien RICHARD <julien.y.richard@gmail.com>
2025-03-10 10:24:33 +0100

    Nettoyage des dubugger et console

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,11 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
-  console.log("First Console Log");
 
   useEffect(() => {
-    console.log("Console frome UseEffect");
     fetch("http://localhost:3000/api/repos")
       .then((res) => res.json())
       .then((repos) => {
-        debugger;
         setData(repos);
       })
       .catch((error) => {
@@ -19,10 +16,8 @@ function App() {
       });
   }, []);
 
-  console.log("Just before return");
   return (
     <>
-      {console.log("In the return", data)}
       <h1>Mon titre</h1>
       {data.length > 0 && <h2>{data[0].url}</h2>}
     </>
